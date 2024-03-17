8f18c5d5873e1b099c9ef656fe059b7c931e8cf0
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-03-17 21:29:28 +0100

    vercelll

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,28 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const roomModel = require("./models/room");
 const dbConfig = require("./db");
 const roomsRoute = require("./routes/roomsRoute");
 const usersRoute = require("./routes/usersRoute");
 const bookingsRoute = require("./routes/bookingsRoute");
+
+// Middleware
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
+// Routes
+app.get("/", (req, res) => res.send("hello"));
 app.use("/api/rooms", roomsRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/bookings", bookingsRoute);
-const port = process.env.PORT || 5001;
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Node Server Started using ${port}`));
+
 module.exports = app;
